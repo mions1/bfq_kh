d020216b754a7b41d245fde475f6e73f3a9146d0
mions1 <simone.mione1@gmail.com>
2020-01-16 10:37:28 +0100

    Fix generico

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -5541,10 +5541,6 @@ static void bfq_exit_bfqq(struct bfq_data *bfqd, struct bfq_queue *bfqq)
 
 	bfq_log_bfqq(bfqd, bfqq, "%p, %d", bfqq, bfqq->ref);
 
-	bfq_put_cooperator(bfqq);
-
-	bfq_put_queue(bfqq); /* release process reference */
-
 	// DONE
 	/*
 	 * TODO Controllare se bugon funziona
@@ -5557,6 +5553,12 @@ static void bfq_exit_bfqq(struct bfq_data *bfqd, struct bfq_queue *bfqq)
 	printk("CHECKING CONSINSTENCY IN TASK LIST \n");
 	BFQ_BUG_ON(hlist_unhashed(&current->task_list_node)); 
 	hlist_del_init(&current->task_list_node);	
+
+	bfq_put_cooperator(bfqq);
+
+	bfq_put_queue(bfqq); /* release process reference */
+
+
 }
 
 static void bfq_exit_icq_bfqq(struct bfq_io_cq *bic, bool is_sync)
