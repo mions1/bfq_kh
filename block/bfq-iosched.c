50cad9845576100e591c1a8540b70c935a855fdb
mions1 <simone.mione1@gmail.com>
2020-01-24 12:30:16 +0100

    Fix generico

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -3137,7 +3137,8 @@ bfq_merge_bfqqs(struct bfq_data *bfqd, struct bfq_io_cq *bic,
 	// TODO: Da eliminare
 	new_bfqq->pid = -1;
 	bfqq->bic = NULL;
-
+	/* release process reference to bfqq */
+	bfq_put_queue(bfqq);
 	// DONE
 	/*
 	 * delete task_list_node from one list to add it to another list
@@ -3149,17 +3150,16 @@ bfq_merge_bfqqs(struct bfq_data *bfqd, struct bfq_io_cq *bic,
 		hlist_add_head(&item->task_list_node, &new_bfqq->task_list);
 		//printk(KERN_CONT "%lu, ",(unsigned long)&item->pid);
 	}
-	/*
+	
 	printk("------BURST LIST START (%i)-------\n", bfqq_process_refs(new_bfqq));
 	hlist_for_each_entry_safe(item, n, &new_bfqq->task_list, task_list_node) 
 	{
 		printk(KERN_CONT "%lu, ",(unsigned long)&item->pid);
 	}
 	printk("------BURST LIST END-------");
-	*/
+	
 
-	/* release process reference to bfqq */
-	bfq_put_queue(bfqq);
+
 
 }
 
