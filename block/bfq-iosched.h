400569bdf7ed1b196d3261e39bd1ea648b3054b0
mions1 <simone.mione1@gmail.com>
2020-01-29 20:55:23 +0100

    Fix generico

--- block/bfq-iosched.h
+++ block/bfq-iosched.h
@@ -1069,7 +1069,17 @@ static inline void bfq_pid_to_str(int pid, char *str, int len, struct bfq_queue 
 		//{
 		//		str += item->pid +', ';
 		//}
-		snprintf(str, len, "SHARED-");
+
+		ssize_t num_char = 0;
+
+		num_char += sprintf(str + num_char, "SHARED-:\n");
+		list_for_each_entry(item, &bfqq->task_list, task_list_node) {
+			num_char += sprintf(str + num_char,
+						"%d, ",
+						item->pid);
+		}
+
+		//snprintf(str, len, "SHARED-");
 	}
 }
 
