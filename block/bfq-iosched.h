fcd19142eed377706d4983ea276836590d2a729f
mions1 <simone.mione1@gmail.com>
2020-01-30 10:44:28 +0100

    Stampe in formato piu carino

--- block/bfq-iosched.h
+++ block/bfq-iosched.h
@@ -1065,21 +1065,14 @@ static inline void bfq_pid_to_str(int pid, char *str, int len, struct bfq_queue 
 	if (false)
 		snprintf(str, len, "%d", pid);
 	else {
-		//hlist_for_each_entry(item, &bfqq->task_list, task_list_node)
-		//{
-		//		str += item->pid +', ';
-		//}
+		ssize_t num_char = 0;
 
-				ssize_t num_char = 0;
-
-		num_char += sprintf(str + num_char, "SHARED-:\n");
+		num_char += sprintf(str + num_char, "SHARED, pid list: ");
 		hlist_for_each_entry(item, &bfqq->task_list, task_list_node) {
-			num_char += sprintf(str + num_char, "allah: \n");
 			num_char += sprintf(str + num_char,
 						"%d, ",
 						item->pid);
 		}
-		//snprintf(str, len, "SHARED-");
 	}
 }
 
